d30cbd2010e29d12d5c15aa5d696c55a5af84db7
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-22 22:07:14 +0000

    zephyr libraries used

--- src/main.c
+++ src/main.c
@@ -1,14 +1,4 @@
-/*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <stdio.h>
-
-int main(void)
-{
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
-	return 0;
-}
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
