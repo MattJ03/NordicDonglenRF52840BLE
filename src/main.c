3c549881b33bec53119a1569fe4e44e805709f9e
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-22 22:21:06 +0000

    fix

--- src/main.c
+++ src/main.c
@@ -2,3 +2,11 @@
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
+
+#define LED_NODE_DT_ALIAS(led0);  //will get the dongle from the DT
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+
+void main(void) {
+	
+}
