559017d221526db99cfa24ea7b9cb7b11291c8ce
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-24 23:52:49 +0000

    established macros

--- src/main.c
+++ src/main.c
@@ -2,3 +2,8 @@
 #include <zephyr/bluetooth/bluetooth.h>
 #include <zephyr/types.h>
 #include <zephyr/bluetooth/hci.h>
+#include <zephyr/blueooth/gap.h>
+#include <zephyr/bluetooth/conn.h>
+
+#define DEVICE_NAME "Zephyr_BLE"  //for the nordic dongle
+#define DEVICE_NAME_LEN (sizeof(DEVICE_NAME) - 1)
