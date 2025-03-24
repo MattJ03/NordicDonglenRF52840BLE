bccac2c40be45213a9a63cec3c9d079b0987cd89
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-03-24 23:47:43 +0000

    modules for bluetooth

--- build/zephyr/isr_tables.c
+++ build/zephyr/isr_tables.c
@@ -58,13 +58,13 @@ uintptr_t __irq_vector_table _irq_vector_table[48] = {
 	((uintptr_t)&_isr_wrapper),
 };
 struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
-	{(const void *)0x8fbf, (ISR)0x8e69}, /* 0 */
+	{(const void *)0x8fd3, (ISR)0x8e7d}, /* 0 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 1 */
-	{(const void *)0x98c8, (ISR)0x8d37}, /* 2 */
+	{(const void *)0x98dc, (ISR)0x8d4b}, /* 2 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 3 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 4 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 5 */
-	{(const void *)0x6349, (ISR)0x8e69}, /* 6 */
+	{(const void *)0x635d, (ISR)0x8e7d}, /* 6 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 7 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 8 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 9 */
@@ -75,7 +75,7 @@ struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 14 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 15 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 16 */
-	{(const void *)0x0, (ISR)0x56f5}, /* 17 */
+	{(const void *)0x0, (ISR)0x5709}, /* 17 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 18 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 19 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 20 */
@@ -97,7 +97,7 @@ struct _isr_table_entry __sw_isr_table _sw_isr_table[48] = {
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 36 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 37 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 38 */
-	{(const void *)0x4195, (ISR)0x8e69}, /* 39 */
+	{(const void *)0x41a9, (ISR)0x8e7d}, /* 39 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 40 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 41 */
 	{(const void *)0x0, (ISR)z_irq_spurious}, /* 42 */
--- src/main.c
+++ src/main.c
@@ -1,25 +1,4 @@
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
-#include <zephyr/drivers/gpio.h>
-#include <stdio.h>
-
-#define LED_NODE DT_ALIAS(led0)  //will get the dongle from the DT, led0 is the first device 
-
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
-
-int main(void) {
-
-	if(!device_is_ready(led.port)) {
-		printf("The device is not in a state to be configured\n");
-		return;  //led not ready try again 
-	}
-
-	gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-
-	while(1) {
-		gpio_pin_toggle_dt(&led);
-		k_msleep(100);
-	}
-
-}
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/types.h>
+#include <zephyr/bluetooth/hci.h>
